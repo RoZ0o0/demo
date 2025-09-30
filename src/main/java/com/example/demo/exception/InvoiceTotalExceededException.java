aae7f4bc85a2df43e531ae7fe3cb56f953c0f8b2
RoZ0o0 <62017852+RoZ0o0@users.noreply.github.com>
2025-09-30 05:39:54 +0200

    refactor: remove redundant exception class

--- src/main/java/com/example/demo/exception/InvoiceTotalExceededException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exception;
-
-public class InvoiceTotalExceededException extends RuntimeException {
-    public InvoiceTotalExceededException(String message) {
-        super(message);
-    }
-}
