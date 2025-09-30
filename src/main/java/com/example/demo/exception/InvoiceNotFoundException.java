712b8646e6cd84a03e57e3be2b4fd58d0034e827
RoZ0o0 <62017852+RoZ0o0@users.noreply.github.com>
2025-09-30 18:04:40 +0200

    fix: handle publicToken in InvoiceNotFoundException

--- src/main/java/com/example/demo/exception/InvoiceNotFoundException.java
+++ src/main/java/com/example/demo/exception/InvoiceNotFoundException.java
@@ -4,4 +4,8 @@ public class InvoiceNotFoundException extends RuntimeException {
     public InvoiceNotFoundException(Long id) {
         super(String.format("Invoice with id '%s' does not exist.", id));
     }
+
+    public InvoiceNotFoundException(String publicToken) {
+        super(String.format("Invoice with public token '%s' does not exists.", publicToken));
+    }
 }
